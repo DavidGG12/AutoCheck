d9db0029498ff93129455ec23d0d7450ca4d6e68
Ian García <garcia.iandavid117@hotmail.com>
2025-10-27 17:51:17 -0600

    ADDING EXECUTION CLASS OF SELENIUM

--- Helpers/SeleniumHelper.py
+++ Helpers/SeleniumHelper.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from GettingSettings import GettingSettings
 from WebDriverFactory import WebDriverFactory
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
+from EventLoggerWin import EventLoggerWin
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,11 +11,30 @@ class SeleniumHelper:
         self._settings = GettingSettings
         self._factoryWebDriver = WebDriverFactory
 
+        self._logger = EventLoggerWin("AutoCheck")
         self._url = self._settings.getUrl()
         self._drive = self._factoryWebDriver.getDriver(option=self._settings.getBrowser())
 
     def execProcess(self):
-        self._drive.get("http://rh.sanborns.com.mx/web/checador")
-        
+        try:
+            self._drive.get("http://rh.sanborns.com.mx/web/checador")
+
+            # Wait for the page to be fully loaded
+            WebDriverWait(self._drive, 15).until(
+                lambda d: d.execute_script("return document.readyState") == "complete"
+            )
+
+            iframe = WebDriverWait(self._drive, 15).until(
+                EC.presence_of_element_located((By.TAG_NAME, "iframe"))
+            )
+            self._drive.switch_to.frame(iframe)
+            txtInput = self._settings.find_element(By.ID, "numeroEmpleado")
+
+            txtInput.send_keys(self._settings.getWorkerNumber())
+            
+            self._logger.throwEvent(2000001, "INFO", "PROCESS WAS EXECUTING SUCCESSFULLY.")
+        except Exception as e:
+            self._logger.throwEvent(5000003, "ERROR", e)
+            raise ValueError(e)
         
 
