7fa1e97d7b0d406b9e651f1d0fb158da6076e4d7
Ian García <garcia.iandavid117@hotmail.com>
2025-10-30 16:06:14 -0600

    ADDING COMMENTS IN EventLoggerWin.py

--- Helpers/EventLoggerWin.py
+++ Helpers/EventLoggerWin.py
@@ -1,8 +1,16 @@
 import win32evtlogutil
 import win32evtlog
 
+'''
+    Summary
+        EventLoggerWin, this class is to have a logger when 
+        the project will be a exe. 'Cause we won't be able
+        to see the console so, the logger we going to put 
+        on Windows Log Viewer.
+'''
 class EventLoggerWin:
     def __init__(self, appName: str):
+        #We need a App name, so this will be a constant that we can see on Log Viewer.
         self.APPNAME = appName
 
     def throwEvent(self, eventID: int, typeEvent: str, message: chr) -> None:
@@ -11,6 +19,8 @@ class EventLoggerWin:
 
             if typeEvent.upper() == "INFO":
                 typeEventLog = win32evtlog.EVENTLOG_INFORMATION_TYPE
+            elif typeEvent.upper() == "SUCCESS":
+                typeEventLog = win32evtlog.EVENTLOG_SUCCESS
             elif typeEvent.upper() == "WARNING":
                 typeEventLog = win32evtlog.EVENTLOG_WARNING_TYPE
             elif typeEvent.upper() == "ERROR":
